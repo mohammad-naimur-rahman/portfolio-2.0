42471aadea2e268e24ad1309a2ff74dd1914d16e
Naimur <naeemhasan28@gmail.com>
2023-11-07 08:08:35 +0600

    fix: build error, add: sharp image optimization package

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -4,9 +4,9 @@ import { ThemeProvider } from 'next-themes'
 import { ReactNode } from 'react'
 
 export function Providers({ children }: { children: ReactNode }) {
-    return (
-        <ThemeProvider enableSystem attribute="class">
-            {children}
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider enableSystem attribute="class">
+      {children}
+    </ThemeProvider>
+  )
 }
