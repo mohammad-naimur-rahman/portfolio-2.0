c2fa753c5d6e1b4e3dcfc39a777340d7f6f6e79d
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-05-19 00:51:17 +0600

    add experience

--- src/constants/homepage/experience/index.ts
+++ src/constants/homepage/experience/index.ts
@@ -11,13 +11,27 @@ export interface IExperience {
 
 export const experiences: IExperience[] = [
   {
+    title: 'Fullstack Web Developer',
+    company: {
+      title: 'Rebel Force Tech Solutions',
+      href: 'https://www.rebelforce.tech/',
+    },
+    start: 'January 2024',
+    end: 'Present',
+    description: [
+      'Created and maintained web applications using Next.js, React, and Tailwind CSS.',
+      'Collaborated with cross-functional teams to ensure seamless project delivery.',
+      'Implemented responsive design and user-friendly interfaces.',
+    ],
+  },
+  {
     title: 'JavaScript/TypeScript Developer',
     company: {
       title: 'Tecooli Service OÜ',
       href: 'https://www.tecooli.com/',
     },
     start: 'June 2021',
-    end: 'Present',
+    end: 'December 2023',
     description: [
       'Spearheaded full-stack development initiatives, contributing to the successful completion of diverse projects, including dashboards, JAM stack applications, and comprehensive full-stack solutions.',
       'Proactively identified and resolved complex challenges, such as debugging and introducing new features into a large and intricate codebase.',
