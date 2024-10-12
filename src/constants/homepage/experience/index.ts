fee0f32f60a6e390cffca5ec2932e71f60f67fda
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-10-13 00:10:38 +0600

    update: experience

--- src/constants/homepage/experience/index.ts
+++ src/constants/homepage/experience/index.ts
@@ -25,13 +25,27 @@ export const experiences: IExperience[] = [
     ],
   },
   {
+    title: 'Fullstack Web Developer',
+    company: {
+      title: 'LyzTech',
+      href: 'https://lyztech.com/',
+    },
+    start: 'October 2022',
+    end: 'January 2024',
+    description: [
+      'Developed new features for Mozilla Hubs VR and Hubs Spoke applications.',
+      'Collaborated with cross-functional teams to ensure seamless project delivery.',
+      'Enhanced application performance by optimizing VR rendering processes.',
+    ],
+  },
+  {
     title: 'JavaScript/TypeScript Developer',
     company: {
       title: 'Tecooli Service OÜ',
       href: 'https://www.tecooli.com/',
     },
     start: 'June 2021',
-    end: 'December 2023',
+    end: 'Sepember 2022',
     description: [
       'Spearheaded full-stack development initiatives, contributing to the successful completion of diverse projects, including dashboards, JAM stack applications, and comprehensive full-stack solutions.',
       'Proactively identified and resolved complex challenges, such as debugging and introducing new features into a large and intricate codebase.',
